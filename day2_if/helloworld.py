db22a09d1de4475b177334e333f241db5926b6f7
faliye <fy15309206903@gmail.com>
2024-05-30 23:18:01 +0900

    deleted:    day2_if/helloworld.py new file:   day2_if_for/helloworld.py

--- day2_if/helloworld.py
+++
@@ -1,8 +0,0 @@
-"""
-day2 if
- 
-version: 0.0.1
-"""
-x = 30
-if 10<= x <= 20:
-    print('boom')
---
+++ day2_if_for/helloworld.py
@@ -0,0 +1,46 @@
+"""
+day2 if
+ 
+version: 0.0.1
+"""
+# x = 10
+# if 18.5 <=  x < 24:
+#     print('perfect!')
+
+# if 18.5 <= x and x < 24:
+#     print('perfect!')
+
+
+# input = int(input("input status"))
+# match input:
+#     case 401: descriction = ''
+#     case _: descriction= '_'
+    
+# print(descriction)
+
+"""
+import random
+
+random.randrange(min, max)
+"""
+
+
+"""
+import time
+
+time.sleep(1)
+"""
+
+"""
+for i in range(cout)
+"""
+
+
+"""
+最大公约数
+"""
+x = int(input('x = '))
+y = int(input('y = '))
+while y % x != 0:
+    x, y = y % x, x # 
+print(f'最大公约数: {x}')
