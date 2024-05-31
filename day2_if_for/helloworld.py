39b85aaf7cd525ae2fed130e13575b9760ca510d
faliye <fy15309206903@gmail.com>
2024-05-31 21:54:59 +0900

    select branch

--- day2_if_for/helloworld.py
+++ day2_if_for/helloworld.py
@@ -39,8 +39,20 @@ for i in range(cout)
 """
 最大公约数
 """
-x = int(input('x = '))
-y = int(input('y = '))
-while y % x != 0:
-    x, y = y % x, x # 
-print(f'最大公约数: {x}')
+# x = int(input('x = '))
+# y = int(input('y = '))
+# while y % x != 0:
+#     x, y = y % x, x # 欧几里得算法
+# print(f'最大公约数: {x}')
+
+
+"""
+fibonacci
+"""
+# a, b = 0, 1
+# for i in range(20):
+#     a, b=b, a+b   
+
+a = 3
+
+print(a ** 2)
