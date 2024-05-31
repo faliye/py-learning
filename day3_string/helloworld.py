e93bab33c3bc493162a74118fc920c766e37d1dd
faliye <fy15309206903@gmail.com>
2024-05-31 21:55:19 +0900

    string

---
+++ day3_string/helloworld.py
@@ -0,0 +1,36 @@
+# string
+# len(string)
+
+# cSring in pString  => boolean
+
+# cSring not in pString  => boolean
+
+# string[start: end: step]
+
+# for _ in rang(len)
+
+# for ele in string:
+
+# find(str, start) # find方法找不到指定的字符串会返回-1，index方法找不到指定的字符串会引发ValueError错误。
+
+# startWith(str)
+
+# endWidth(str)
+
+# str.isdigit()
+
+# str.isalpha()
+
+# str.isalnum()
+
+# str.center(len, str) # wrap by str, to len
+
+# str.rjust(len , str) # just to right
+
+# str.ljust(len , str) # just to left
+
+# str.zfill(len) # 正负号的时候 将符号位放在最前方
+
+print('s'.zfill(10))
+print('+s'.zfill(10)) # +00000000s
+print('-s'.zfill(10)) # -00000000s
