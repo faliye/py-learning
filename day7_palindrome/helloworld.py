2935678f2b657d7a169951df625846fbe9c2cbc0
faliye <fy15309206903@gmail.com>
2024-06-04 22:25:21 +0900

    回文数

---
+++ day7_palindrome/helloworld.py
@@ -0,0 +1,28 @@
+
+from collections import Counter
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        sign=False
+        result = Counter([i for i in s])
+        print(result.values())
+        for i in result.values():
+            if(i%2 ==0):
+                count+=i
+            else:
+                count+=i-1
+                sign = True
+        if(sign):
+            return count+1
+        return count
+        
+print(Solution().longestPalindrome("abccccdd"))
+print(Solution().longestPalindrome("bananas"))
+print(Solution().longestPalindrome("ccc"))
+print(Solution().longestPalindrome("cc"))
+print(Solution().longestPalindrome("c"))
