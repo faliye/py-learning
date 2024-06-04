77f8e9e5865bc2d43847f5078f99ec28441d480e
faliye <fy15309206903@gmail.com>
2024-06-04 22:33:48 +0900

    better

--- day7_palindrome/helloworld.py
+++ day7_palindrome/helloworld.py
@@ -25,3 +25,26 @@ print(Solution().longestPalindrome("bananas"))
 print(Solution().longestPalindrome("ccc"))
 print(Solution().longestPalindrome("cc"))
 print(Solution().longestPalindrome("c"))
+
+
+
+# class Solution(object):
+#     def longestPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         dist = {}
+#         for i in s:
+#             dist[i] = dist.get(i,0)+1
+#         count = 0
+#         sign=False
+#         for i in dist.values():
+#             if(i%2 ==0):
+#                 count+=i
+#             else:
+#                 count+=i-1
+#                 sign = True
+#         if(sign):
+#             return count+1
+#         return count
