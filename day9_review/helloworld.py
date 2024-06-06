ad34a1a2917cbc4ba8ec3d455cbdbc895883f06e
faliye <fy15309206903@gmail.com>
2024-06-06 20:37:51 +0900

    review

---
+++ day9_review/helloworld.py
@@ -0,0 +1,34 @@
+# """
+
+# circle round
+
+# """
+
+# radius = float(input("please input radius"))
+
+# perimeter = 2 * 3.1415* radius
+
+# print(perimeter)
+
+
+# """
+
+# BMI
+
+# """
+
+# height = float(input('heigh(cm)：'))
+# weight = float(input('weight(kg)：'))
+# bmi = weight / (height / 100) ** 2
+# print(f'{bmi = :.1f}')
+# if 18.5 <= bmi < 24:
+#     print('你的身材很棒！')
+    
+    
+# import time
+
+# print('hello, world')
+# time.sleep(1)
+
+
+# print(sum(range(2, 101, 2),))
