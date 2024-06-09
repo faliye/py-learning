e86fac534b78a47ffeb01e04a50ced2642e5076f
faliye <fy15309206903@gmail.com>
2024-06-09 21:55:44 +0900

    feat: import module

---
+++ day_13/helloworld.py
@@ -0,0 +1,15 @@
+from math import factorial
+
+print(factorial(1) ,factorial(5))
+
+import module as m
+import module1 as m1
+
+m.hello()
+m1.hello()
+
+from module import hello as h
+from module1 import hello as h1
+
+h1()
+h()
